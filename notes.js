28c6f6ae9ce84b166e7084fbd3a0bab3d9905706
Ryan <rbuchanan.prsvr@gmail.com>
2024-04-24 21:03:45 -0400

    first commit

---
+++ notes.js
@@ -0,0 +1,34 @@
+// Create two functions, add3Points and remove1Points(), and have them equals 10
+
+
+// let myPoints = 3
+// function add3Points() {
+//     myPoints += 3
+// }
+// function remove1Points() {
+//     myPoints -= 1
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Points()
+// remove1Points()
+// console.log(myPoints)
+
+// console.log("2" + 2) = "22"
+// console.log(11 + 7) = 18
+// console.log(6 + "5") = "65"
+// console.log("My points: " + 5 + 9) = "59"
+// console.log(2 + 2) = 4
+// console.log("11" + "14") = "1114"
+
+
+                //this is the notes js
+
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph)
+
+// function purchase() {
+//     console.log("button clicked")
+//     errorParagraph.textContent = "Something went wrong please try again"
+// }
---
+++ script.js
@@ -0,0 +1,13 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num1-el").textContent = num2
+
+let add =document.getElementById("add")
+let subtract =document.getElementById("subtract")
+let multiply =document.getElementById("multiply")
+let divide =document.getElementById("divide")
+
+function add() {
+    console.log("add clicked")
+}
